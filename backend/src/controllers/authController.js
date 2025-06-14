8e01a3ebb4385d248fffdd12cb07ee926791ae66
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-14 20:07:48 -0300

    fix(auth): correct bcrypt import and ensure JWT_SECRET is defined

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
-const bycypt = require("bcrypt");   
+const bycrypt = require("bcrypt");   
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const JWT_SECRET = process.env.JWT_SECRET || "mysecret";
 
 exports.register = async (req, res) =>{
       const {username, email, password} = req.body;
@@ -19,6 +20,26 @@ exports.register = async (req, res) =>{
             });
       } catch (error) {
             console.error("Error during registration:", error);
+            res.status(500).json({message: "Internal server error"});
+      }
+}
+
+exports.login = async(req, res) =>{
+      const {email, username, password} = req.body;
+      try{
+            const user = await User.findUnique({
+                  where: email ? { email } : { username: { username } }
+            });
+            if (!user) return res.status(400).json({message: "Invalid credentials"});
+
+            const isMatch = await bycrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({message: "Invalid credentials"});
+
+            const token  = jwt.sign({userId : user.id, email: user.email}, JWT_SECRET, { expiresIn: "1h"});
+
+            res.json({token, user: {id: user.id, email: user.email, username: user.username}});
+      } catch (error) {
+            console.error("Error during login:", error);
             res.status(500).json({message: "Internal server error"});
       }
 }
