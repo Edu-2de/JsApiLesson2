1233086bd684a4b2e2b00762c6958a44dbfff4c5
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-14 20:35:33 -0300

    fix(product): correct typo in findUnique method and improve error handling in registerProduct

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -7,7 +7,7 @@ const prisma = require('../prismaClient');
 export const registerProduct = async (req, res) => {
       const {name, description, price, category, stock} = req.body;
       try{
-            const productExists = await prisma.product.findUnnique({where : {name}});
+            const productExists = await prisma.product.findUnique({where : {name}});
             if(productExists){
                   return res.status(400).json({message: "Product already exists"});
             }
@@ -22,6 +22,21 @@ export const registerProduct = async (req, res) => {
                   }
             })
       }catch(error){
-            return res.status(500).json({message: "Error during product registration:", error});
-
+            console.error("Error during product registration:", error);
+            res.status(500).json({message: "Internal server error"});
       }}
+
+export const getProductById = async (req, res) =>{
+      const {id} = req.params;
+      try{
+            const product = await prisma.product.findUnique({where: {id: parseInt(id)}});
+            if (!product){
+                  return res.status(404).json({message: "Product not found"});
+            }
+            res.status(200).json(product)
+
+      }catch(error){
+            console.error("Error fetching product:", error);
+            res.status(500).json({message: "Internal server error"});
+      }
+}
