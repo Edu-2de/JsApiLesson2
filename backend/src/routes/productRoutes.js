77e1a3775506887b1bfe2ee44c2e9158e8bb4657
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-23 20:47:40 -0300

    feat(api): add product route handlers for retrieving all products, searching, and filtering by category

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { registerProduct} from '../controllers/productController.js';
+import { registerProduct, getAllProducts, searchProducts, getProductsByCategory} from '../controllers/productController.js';
+// 
 
 const router = Router();
 
 router.post('/register', registerProduct);
+router.get('/', getAllProducts);
+router.get('/search', searchProducts);
+router.get('/category/:category', getProductsByCategory);
 
 export default router;
