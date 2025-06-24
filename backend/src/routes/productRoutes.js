fda55ac75d95c9e4914ca607f1204996300f51f7
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-24 11:13:54 -0300

    feat(api): enforce admin role requirement for product registration, update, and deletion routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { registerProduct, getAllProducts, searchProducts, getProductsByCategory, getProductById, updateProduct, deleteProduct} from '../controllers/productController.js';
+import requireAdmin from "../middlewares/adminMiddleware.js";
 
 const router = Router();
 
-router.post('/register', registerProduct);
+router.post('/register', requireAdmin, registerProduct);
 router.get('/', getAllProducts);
 router.get('/search', searchProducts);
 router.get('/category/:category', getProductsByCategory);
 router.get('/:id', getProductById);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.put('/:id', requireAdmin, updateProduct);
+router.delete('/:id', requireAdmin, deleteProduct);
 
 
 export default router;
