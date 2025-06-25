fe670870b0fc0a04961495192e0a08ce2b92e8e4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-25 12:44:13 -0300

    feat(api): update PUT and PATCH methods to include authorization headers

--- frontend/pages/api/products/[id]/index.ts
+++ frontend/pages/api/products/[id]/index.ts
@@ -26,7 +26,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const body = req.body;
     const response = await fetch(`${apiUrl}/api/products/${id}`, {
       method: req.method,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        authorization: Array.isArray(req.headers.authorization)
+          ? req.headers.authorization[0]
+          : req.headers.authorization ||
+            (Array.isArray(req.headers.Authorization)
+              ? req.headers.Authorization[0]
+              : req.headers.Authorization) ||
+            "",
+      },
       body: JSON.stringify(body),
     });
     const data = await response.json();
