f73e7fcc709e8cb1add721a09d602a29f3632b4b
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-06 13:00:03 -0300

    fix: correct syntax errors in schema.sql and enhance account_types table structure

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(20) DEFAULT 'user' CHECK(role IN ('full_access', 'limit_access', 'user')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 INSERT INTO users (name, email, age, password_hash, role) VALUES
 ('bank manager', 'admin@system.com', 30, '$2b$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'full_access')
@@ -24,10 +24,19 @@ ON CONFLICT (email) DO NOTHING;
 CREATE TABLE IF NOT EXISTS account_types (
     id SERIAL PRIMARY KEY,
     type VARCHAR(20) DEFAULT 'current' CHECK(type IN ('current', 'savings', 'premium', 'kids')),
-    daily_limit DECIMAL(10,2) DEFAULT 0.00,
-    monthly_limit DECIMAL(10,2) DEFAULT 0.00,
-    fees INTEGER DEFAULT 0,
-    interest INTEGER DEFAULT 0
-)
+    daily_withdrawal_limit DECIMAL(10,2) DEFAULT 0.00,
+    daily_transfer_limit DECIMAL(10,2) DEFAULT 0.00,
+    monthly_withdrawal_limit DECIMAL(10,2) DEFAULT 0.00,
+    monthly_transfer_limit DECIMAL(10,2) DEFAULT 0.00
+    
+);
 
 
+CREATE TABLE IF NOT EXISTS interest_and_fees (
+    id SERIAL PRIMARY KEY,
+    account_type_id INTEGER REFERENCES account_types(id) ON DELETE SET NULL,
+    monthly_maintenance_fee DECIMAL(5,2) DEFAULT 0.00,
+    withdrawal_fee DECIMAL(5,2) DEFAULT 0.00,
+    transfer_fee DECIMAL(5,2) DEFAULT 0.00,
+    interest_rate  DECIMAL(5,2) DEFAULT 0.00
+);
