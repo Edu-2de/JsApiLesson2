5f35e549e8fd170f11dbaca505e54f3bc1447105
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-10 13:44:39 -0300

    fix: improve user existence and account validation in registerAccount method

--- src/controllers/auccontController.ts
+++ src/controllers/auccontController.ts
@@ -87,15 +87,23 @@ export class AccountController{
             const existingUser = await pool.query(
                 `SELECT id FROM users WHERE id = $1`,
                 [user_id]);
-            if (existingUser.rows.length < 0 || existingUser.rows.length > 0){
-                res.status(400).json({message: 'This user not exists or already have a account'});
+            if(existingUser.rows.length === 0){
+                res.status(400).json({message: 'This user not exists'});
+                return;
+            }
+            const existingAccountUser = await pool.query(
+                `SELECT user_id FROM accounts WHERE user_id = $1`,
+                [user_id]
+            )
+            if ( existingAccountUser.rows.length > 0){
+                res.status(400).json({message: 'This user already have a account'});
                 return;
             }
 
             const existingType = await pool.query(
                 `SELECT id from account_types WHERE id = $1`,
                 [account_type_id]);
-            if (existingType.rows.length < 0){
+            if (existingType.rows.length === 0){
                 res.status(400).json({message: 'This type not exists in table'})
                 return;
             }
