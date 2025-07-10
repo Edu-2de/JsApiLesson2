6e57b1a69908cc4fe76f812631c1c254302cae81
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-10 19:05:45 -0300

    feat: add authentication middleware to myaccount and updateAccountById routes

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { AccountController } from "../controllers/accountController";
+import { AccountMiddleware } from "../middleware/accountMiddleware";
 
 const router = Router();
 
 router.post("/login", AccountController.loginAccount);
 router.post("/register", AccountController.registerAccount);
+
+router.get('/myaccount', AccountMiddleware.authenticateToken,  AccountController.getAccount);
+
+router.patch('/:accountId', AccountMiddleware.authenticateToken, AccountMiddleware.requireAdminOrOwner, AccountController.updateAccountById);
 
 export default router;
