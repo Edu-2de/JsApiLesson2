5a096c23236c025855ed5bf87a96604250541093
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-10 18:37:17 -0300

    fix: enhance updateAccountAdmin response with success and error messages

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -227,8 +227,15 @@ export class AccountController {
                 [account_type_id, balance, status, accountId]
             );
 
-            res.json
-            
+            res.json({
+                message: 'Account updated successfully',
+                account: result.rows[0]
+            });
+        }catch(error){
+            res.status(500).json({
+                message: 'Error updating account',
+                error: error instanceof Error ? error.message : String(error)
+            })
         }
 
     }
