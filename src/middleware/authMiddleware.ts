e4d384543b421e43d268333230a1de09b341f5c1
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-10 14:32:57 -0300

    feat: implement AuthMiddleware with token authentication structure

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,2 +1,20 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+
+interface AuthRequest extends Request{
+    user?:{
+        id: number;
+        email: string;
+        role: string;
+
+    }
+}
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
+
+export class AuthMiddleware{
+    static authenticateToken = (req: AuthRequest, res: Response, next: NextFunction): void =>{
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+    }
+}
