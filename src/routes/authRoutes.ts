1563bbffe6302fda9f99f3a585b2c35773091f71
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-11 14:16:34 -0300

    fix: add authentication middleware to profile and admin routes for enhanced security

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/authController';
-
+import { AuthMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/login', AuthController.login);
 router.post('/register', AuthController.register);
 
-router.get('/profile', AuthController.getUser);
-router.get('/admin/:userId', AuthController.getUser);
+router.get('/profile', AuthMiddleware.authenticateToken ,AuthController.getUser);
+router.get('/admin/:userId', AuthMiddleware.requireAdmin, AuthController.getUser);
 
 export default router;
