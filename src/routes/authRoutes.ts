298b77558e25bd570069a80242bbfbcf7d566510
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-11 14:15:33 -0300

    feat: add routes for user profile and admin user retrieval in authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/authController';
 
+
 const router = Router();
 
 router.post('/login', AuthController.login);
 router.post('/register', AuthController.register);
+
+router.get('/profile', AuthController.getUser);
+router.get('/admin/:userId', AuthController.getUser);
 
 export default router;
