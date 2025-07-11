2f66602cb4bfdbc1e19a4f52babaa9eda58c271b
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-11 14:20:38 -0300

    fix: correct route path for retrieving all accounts in accountRoutes

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -14,7 +14,6 @@ router.delete('/myaccount', AccountMiddleware.requireAdminOrOwner, AccountContro
 router.get('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.getAccountById);
 router.patch('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.updateAccountById);
 router.delete('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.deleteAccountById);
-
-router.get('/all', AccountMiddleware.requireAdminOrOwner, AccountController.getAllAccounts);
+router.get('admin/all', AccountMiddleware.requireAdmin, AccountController.getAllAccounts);
 
 export default router;
