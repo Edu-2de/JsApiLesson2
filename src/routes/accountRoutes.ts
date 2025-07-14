39534d2c7cbde55113c013ac2411227fb78d847f
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-14 17:56:54 -0300

    fix: reorganize routes for better clarity and maintainability

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -7,12 +7,22 @@ const router = Router();
 router.post('/login', AccountController.loginAccount);
 router.post('/register', AccountController.registerAccount);
 
+// -------- need login account -----------
 router.get('/myaccount', AccountMiddleware.authenticateToken, AccountController.getAccount);
+
 router.delete('/myaccount', AccountMiddleware.requireAdminOrOwner, AccountController.deleteAccount);
+router.patch('/myaccount/close', AccountMiddleware.requireAdminOrOwner, AccountController.CloseAccount);
+router.patch('/myaccount/active', AccountMiddleware.requireAdminOrOwner, AccountController.ActiveAccount);
+
+// -------- need admin access -----------
+router.delete('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.deleteAccountById);
+
+router.patch('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.updateAccountById);
+router.patch('/admin/block/:accountId', AccountMiddleware.requireAdmin, AccountController.BlockAccountId);
+router.patch('/admin/close/:accountId', AccountMiddleware.requireAdmin, AccountController.CloseAccountId);
+router.patch('/admin/active/:accountId', AccountMiddleware.requireAdmin, AccountController.ActiveAccountId);
 
 router.get('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.getAccountById);
-router.patch('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.updateAccountById);
-router.delete('/admin/:accountId', AccountMiddleware.requireAdmin, AccountController.deleteAccountById);
-router.get('admin/all', AccountMiddleware.requireAdmin, AccountController.getAllAccounts);
+router.get('/admin/all', AccountMiddleware.requireAdmin, AccountController.getAllAccounts);
 
 export default router;
