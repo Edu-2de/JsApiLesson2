a8ac73b404a560eded6229a7dbb679a31c3ddd80
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-15 13:05:56 -0300

    feat: add success response test for activating an account in activeAccountId

--- src/__tests__/accountController.test.ts
+++ src/__tests__/accountController.test.ts
@@ -951,5 +951,48 @@ describe('AccountController', () => {
       expect(mockRes.status).toHaveBeenCalledWith(400);
       expect(mockRes.json).toHaveBeenCalledWith({ error: 'This account is already active!' });
     });
+
+    it('should return success response', async () => {
+      mockReq.params = { accountId: 1 };
+      const mockAccount = {
+        id: 1,
+        balance: 0.0,
+        account_number: '001-12345-6',
+        status: 'closed',
+        user: {
+          name: 'Miguel',
+          email: 'miguel@gmail.com',
+          age: 20,
+          role: 'user',
+        },
+        account_type: {
+          type: 'current',
+          daily_withdrawal_limit: 1000.0,
+          daily_transfer_limit: 5000.0,
+        },
+      };
+
+      mockPool.query.mockResolvedValueOnce({ rows: [mockAccount] }).mockResolvedValueOnce({
+        rows: [
+          {
+            id: 1,
+            status: 'active',
+            updated_at: '2025',
+          },
+        ],
+      });
+
+      await AccountController.activeAccountId(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(201);
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: 'Account activated successfully',
+        account: {
+          id: 1,
+          status: 'active',
+          account_number: '001-12345-6',
+        },
+      });
+    });
   });
 });
