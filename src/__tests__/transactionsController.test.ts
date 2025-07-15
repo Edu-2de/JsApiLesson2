cc1cd9bf4187910f3941d4dc9d74656da250ff14
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-15 17:13:43 -0300

    feat: add test for handling zero withdrawal amount in TransactionsController

--- src/__tests__/transactionsController.test.ts
+++ src/__tests__/transactionsController.test.ts
@@ -40,5 +40,35 @@ describe('TransactionsController', () => {
       expect(mockRes.status).toHaveBeenCalledWith(400);
       expect(mockRes.json).toHaveBeenCalledWith({ error: 'withdrawal is missing' });
     });
+
+    it('should be return 400 if withdrawal cannot be negative or equal to zero', async () => {
+      mockReq.account = { id: 1 };
+      const mockAccount = {
+        id: 1,
+        balance: 0.0,
+        account_number: '001-12345-6',
+        status: 'blocked',
+        user: {
+          name: 'Miguel',
+          email: 'miguel@gmail.com',
+          age: 20,
+          role: 'user',
+        },
+        account_type: {
+          type: 'current',
+          daily_withdrawal_limit: 1000.0,
+          daily_transfer_limit: 5000.0,
+        },
+      };
+
+      mockPool.query.mockResolvedValueOnce({ rows: [mockAccount] });
+
+      mockReq.body = { withdrawal: 0 };
+
+      await TransactionsController.withdrawal(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({ error: 'withdrawal cannot be negative or equal to zero' });
+    });
   });
 });
