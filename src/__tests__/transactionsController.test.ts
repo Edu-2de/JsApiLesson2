88c30f376e9b90c72086974a677bbab36b5e1524
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-15 17:21:11 -0300

    feat: add test for withdrawal exceeding account balance in TransactionsController

--- src/__tests__/transactionsController.test.ts
+++ src/__tests__/transactionsController.test.ts
@@ -70,5 +70,36 @@ describe('TransactionsController', () => {
       expect(mockRes.status).toHaveBeenCalledWith(400);
       expect(mockRes.json).toHaveBeenCalledWith({ error: 'withdrawal cannot be negative or equal to zero' });
     });
+
+    it('should be return 400 if withdrawal cannot be negative or equal to zero', async () => {
+      mockReq.account = { id: 1 };
+      const mockAccount = {
+        id: 1,
+        balance: 10.00,
+        account_number: '001-12345-6',
+        status: 'blocked',
+        user: {
+          name: 'Miguel',
+          email: 'miguel@gmail.com',
+          age: 20,
+          role: 'user',
+        },
+        account_type: {
+          type: 'current',
+          daily_withdrawal_limit: 1000.0,
+          daily_transfer_limit: 5000.0,
+        },
+        withdrawal_fee:1.00
+      };
+
+      mockPool.query.mockResolvedValueOnce({ rows: [mockAccount] });
+
+      mockReq.body = { withdrawal: 10.00 };
+
+      await TransactionsController.withdrawal(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({ error: 'withdrawal is bigger than balance' });
+    });
   });
 });
