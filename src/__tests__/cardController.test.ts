45c843fa8a3b05c255e96aeba25d8f3fb891a22a
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-16 19:00:40 -0300

    test: add additional test case for createCard method to handle inactive accounts

--- src/__tests__/cardController.test.ts
+++ src/__tests__/cardController.test.ts
@@ -30,13 +30,44 @@ describe('CardController.test', () => {
   describe('createCard', () => {
     it('should be return 400 if account not exist', async () => {
       mockReq.account = { id: 1 };
-
+    
       mockPool.query.mockResolvedValueOnce({ rows: [] });
 
       await CardController.createCard(mockReq, mockRes);
 
       expect(mockRes.status).toHaveBeenCalledWith(400);
       expect(mockRes.json).toHaveBeenCalledWith({ error: 'This account not exist' });
+    });
+
+    it('should be return 400 if the account is not active', async () => {
+      mockReq.account = { id: 1 };
+      const mockAccount = {
+        id: 1,
+        balance: 10.0,
+        account_number: '001-12345-6',
+        status: 'closed',
+        account_type_id: 1,
+        transfer_fee: 1.0,
+        user: {
+          name: 'Miguel',
+          email: 'miguel@gmail.com',
+          age: 20,
+          role: 'user',
+        },
+        account_type: {
+          id: 1,
+          type: 'current',
+          daily_withdrawal_limit: 1000.0,
+          daily_transfer_limit: 5000.0,
+        },
+      };
+
+      mockPool.query.mockResolvedValueOnce({ rows: [mockAccount] });
+
+      await CardController.createCard(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({ error: 'The account must be active for this action' });
     });
   });
 });
