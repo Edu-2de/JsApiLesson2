9c3103dc0825bad7318446332cf543f768f69234
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-16 10:42:00 -0300

    feat: implement account validation and query in createCard method of CardController

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -7,10 +7,22 @@ const JWT_SECRET = process.env.JWT_SECRET || '';
 
 export class CardController {
   static createCard = async (req: AccountAuthRequest, res: Response) => {
-    try{
-      
-    }catch(error){
+    try {
+      if (!req.account) {
+        res.status(400).json({ error: 'Account is missing!' });
+        return;
+      }
+      const accountId = req.account.id;
 
-    }
-  }
+      const result = await pool.query(
+        `SELECT 
+          a.id, a.account_type_id, a.balance, a.status
+          fees.withdrawal_fee
+          FROM accounts a
+          INNER JOIN interest_and_fees fees ON a. = fees.id 
+        WHERE a.id = $1`,
+        [accountId]
+      );
+    } catch (error) {}
+  };
 }
